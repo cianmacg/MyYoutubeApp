cf12fdcbb9669fe6c87f2567741d8ac95a2f1031
CianuReeves <scian94@gmail.com>
2017-05-09 14:33:33 +0100

    Initial Commit

---
+++ app/src/main/java/cianmacg/myyoutubeapp/Config.java
@@ -0,0 +1,14 @@
+package cianmacg.myyoutubeapp;
+
+/**
+ * Created by Cian on 08/05/2017.
+ */
+
+public final class Config {
+
+    private Config() {
+    }
+
+    public static final String YOUTUBE_API_KEY = "AIzaSyDN0YYdId8It5bn9FGKoGBINIjYC1vYPKA";
+
+}
---
+++ app/src/main/java/cianmacg/myyoutubeapp/MainActivity.java
@@ -0,0 +1,36 @@
+package cianmacg.myyoutubeapp;
+
+import android.os.Bundle;
+
+import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerView;
+
+public class MainActivity extends YouTubeBaseActivity implements YouTubePlayer.OnInitializedListener{
+
+    private static final int RECOVERY_REQUEST = 1;
+
+    private YouTubePlayerView youTubeView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        youTubeView = (YouTubePlayerView) findViewById(R.id.youtube_view);
+        youTubeView.initialize("AIzaSyDN0YYdId8It5bn9FGKoGBINIjYC1vYPKA", this);
+
+    }
+
+    @Override
+    public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
+
+    }
+
+    @Override
+    public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+
+    }
+
+}
